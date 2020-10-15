c8d7d4140e21f93013b980b3b45b28602658aacb
andrgje <aftgjesdal@gmail.com>
2020-10-15 15:40:52 +0200

    almost working(i think)

--- ass3/threads.c
+++ ass3/threads.c
@@ -10,8 +10,10 @@ typedef struct {
 } int_padded;
 
 typedef struct {
-  const float **v;
-  float **w;
+  const float **re_matrix;
+  const float **im_matrix;
+  int **root;
+  int **iterations;
   int ib;
   int istep;
   int sz;
@@ -22,8 +24,10 @@ typedef struct {
 } thrd_info_t;
 
 typedef struct {
-  const float **v;
-  float **w;
+  const float **re_matrix;
+  const float **im_matrix;
+  int **root;
+  int **iterations;
   int sz;
   int nthrds;
   mtx_t *mtx;
@@ -37,8 +41,10 @@ main_thrd(
     )
 {
   const thrd_info_t *thrd_info = (thrd_info_t*) args;
-  const float **v = thrd_info->v;
-  float **w = thrd_info->w;
+  const float **re_matrix = thrd_info->re_matrix;
+  const float **im_matrix=thrd_info->im_matrix;
+  int **root = thrd_info->root;
+  int **iterations=thrd_info->iterations;
   const int ib = thrd_info->ib;
   const int istep = thrd_info->istep;
   const int sz = thrd_info->sz;
@@ -48,15 +54,15 @@ main_thrd(
   int_padded *status = thrd_info->status;
 
   for ( int ix = ib; ix < sz; ix += istep ) {
-    const float *vix = v[ix];
+    //const float *vix=v[ix];
     // We allocate the rows of the result before computing, and free them in another thread.
-    float *wix = (float*) malloc(sz*sizeof(float));
-
+    int *loc_root = (int*) malloc(sz*sizeof(int));
+    int *loc_iterations=(int*) malloc(sz*sizeof(int));
     for ( int jx = 0; jx < sz; ++jx )
-      wix[jx] = sqrtf(vix[jx]);
-
+      //wix[jx] = sqrtf(vix[jx]);
+      loc_root[ix]=ix;
     mtx_lock(mtx);
-    w[ix] = wix;
+    root[ix] = loc_root;
     status[tx].val = ix + istep;
     mtx_unlock(mtx);
     cnd_signal(cnd);
@@ -64,7 +70,10 @@ main_thrd(
     // In order to illustrate thrd_sleep and to force more synchronization
     // points, we sleep after each line for one micro seconds.
     thrd_sleep(&(struct timespec){.tv_sec=0, .tv_nsec=1000}, NULL);
+    free(loc_root);
+    free(loc_iterations);
   }
+  
 
   return 0;
 }
@@ -75,8 +84,8 @@ main_thrd_check(
     )
 {
   const thrd_info_check_t *thrd_info = (thrd_info_check_t*) args;
-  const float **v = thrd_info->v;
-  float **w = thrd_info->w;
+  int **root = thrd_info->root;
+  int **iterations = thrd_info->iterations;
   const int sz = thrd_info->sz;
   const int nthrds = thrd_info->nthrds;
   mtx_t *mtx = thrd_info->mtx;
@@ -108,9 +117,9 @@ main_thrd_check(
       // Instead of employing a conditional variable, we could also invoke
       // thrd_yield or thrd_sleep in order to yield to other threads or grant a
       // specified time to the computation threads.
-    }
+    
 
-    fprintf(stderr, "checking until %i\n", ibnd);
+      fprintf(stderr, "checking until %i\n", ibnd);
 
     // We do not initialize ix in this loop, but in the outer one.
     for ( ; ix < ibnd; ++ix ) {
@@ -118,16 +127,16 @@ main_thrd_check(
       // situation where the check thread completes fast than the computaton
       // threads.
       int jx = sz-1;
-      float diff = v[ix][jx] - w[ix][jx] * w[ix][jx];
-      if ( diff < -eps || diff > eps ) {
-        fprintf(stderr, "incorrect compuation at %i %i: %f %f %f\n",
-            ix, jx, diff, v[ix][jx], w[ix][jx]);
+      //float diff = v[ix][jx] - w[ix][jx] * w[ix][jx];
+      //if ( diff < -eps || diff > eps ) {
+      fprintf(stderr, "%i %i: %i\n",ix, jx, root[ix][jx]);
         // This exists the whole program, including all other threads.
         exit(1);
       }
 
       // We free the component of w, since it will never be used again.
-      free(w[ix]);
+      free(root[ix]);
+      free(iterations[ix]);
     }
   }
 
@@ -135,7 +144,7 @@ main_thrd_check(
 }
 
 int
-main()
+main(int argc, char* argv[])
 {
   
   //Parse command line args
@@ -146,34 +155,56 @@ main()
   strcpy(arg1,argv[1]);
   strcpy(arg2,argv[2]);
 
-  if(arg1+1=='t'){
-    const size_t nrthreads=atoi(arg1+2);
-    const size_t sz=atoi(arg2+2);
+  int nthrds;
+  int sz;
+  
+  if(argv[1][1]=='t'){
+    nthrds=atoi(arg1+2);
+    sz=atoi(arg2+2);
+  }else if(argv[2][1]=='t'){
+    nthrds=atoi(arg2+2);
+    sz=atoi(arg1+2);
   }else{
-    const size_t nrthreads=atoi(arg2+2);
-    const size_t sz=atoi(arg1+2);
+    printf("Erroer parsin cmd line args\n");
+    return -1;
   }
 
-  size_t degree=atoi(argv[3]);
+  printf("%i,%i",nthrds,sz);
+
+  int degree=atoi(argv[3]);
 
 
-  //Initialize coordinate vector
-  float *coordinates = (float**) malloc(sz*sizeof(float*));
-
-
-  //Initialize result matrices
-  int **iter
+  //Initialize coordinate matrices
+  float **re_matrix=(float**) malloc(sz*sizeof(float*));
+  float **im_matrix=(float**) malloc(sz*sizeof(float*));
+  float *re_entries=(float*) malloc(sz*sz*sizeof(float));
+  float *im_entries=(float*) malloc(sz*sz*sizeof(float));
 
   
   //Calculating gap between coordinates
-  float gap=4./float(sz);
+  float gap=4./(sz-1);
 
 
-  //Filling in vector
-  for ( int ix = 0, jx = 0; ix < sz; ++ix, jx += sz )
-    coordinates[ix]=-2+ix*gap
- 
+  //Filling in coordinates
+  for ( int ix = 0, jx = 0; ix < sz; ++ix, jx += sz ){
+    re_matrix[ix]=re_entries+jx;
+    im_matrix[ix]=im_entries+jx;
 
+  }
+  for(int ix=0;ix<sz;++ix){
+    for(int jx=0;jx<sz;++jx){
+      re_matrix[jx][ix]=-2+ix*gap;
+      im_matrix[ix][jx]=2-ix*gap;
+    }
+  }
+  
+  //Initialize result matrices
+  int **root=(int**) malloc(sz*sizeof(int*));
+  int **iterations=(int**) malloc(sz*sizeof(int*));
+
+
+
+  //Initializing threads
   thrd_t thrds[nthrds];
   thrd_info_t thrds_info[nthrds];
 
@@ -189,8 +220,10 @@ main()
   int_padded status[nthrds];
 
   for ( int tx = 0; tx < nthrds; ++tx ) {
-    thrds_info[tx].coordinates = (const float*) coordinates;
-    thrds_info[tx].iter=
+    thrds_info[tx].re_matrix = (const float**) re_matrix;
+    thrds_info[tx].im_matrix= (const float**) im_matrix;
+    thrds_info[tx].root=root;
+    thrds_info[tx].iterations=iterations;
     thrds_info[tx].ib = tx;
     thrds_info[tx].istep = nthrds;
     thrds_info[tx].sz = sz;
@@ -209,11 +242,19 @@ main()
   }
 
   {
-    thrd_info_check.v = (const float**) v;
-    thrd_info_check.w = w;
+    thrd_info_check.re_matrix = (const float**) re_matrix;
+    thrd_info_check.im_matrix = (const float**) im_matrix;
+    thrd_info_check.root=root;
+    thrd_info_check.iterations=iterations;
     thrd_info_check.sz = sz;
     thrd_info_check.nthrds = nthrds;
     thrd_info_check.mtx = &mtx;
+
+#define Tol_MIN 0.001
+#define Tol_MAX 10000000000.
+#define VALUE 0
+#define DEFAULT_VALUE -1 // 0 ?
+
     thrd_info_check.cnd = &cnd;
     // It is important that we have initialize status in the previous for-loop,
     // since it will be consumed by the check threads.
@@ -225,3 +266,11 @@ main()
       exit(1);
     }
   }
+  free(re_matrix);
+  free(im_matrix);
+  free(re_entries);
+  free(im_entries);
+  free(root);
+  free(iterations);
+  
+}
